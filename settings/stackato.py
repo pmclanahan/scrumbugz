4b96990fe0e981ef39641e3a9d4f6b8b2dbb35a3
Paul McLanahan <paul@mclanahan.net>
2013-01-04 00:59:56 -0500

    Fix site to work with stackato!

--- settings/stackato.py
+++ settings/stackato.py
@@ -1,11 +1,14 @@
 from __future__ import absolute_import
 
+import json
 import os
 
 import dj_database_url
 
 from .base import *
 
+
+SERVICES = json.loads(os.getenv('VCAP_SERVICES', '[]'))
 SITE_URL = 'http://scrumbugz.paas.allizom.org'
 
 ADMINS = (
@@ -15,13 +18,15 @@ MANAGERS = ADMINS
 
 DATABASES = {'default': dj_database_url.config()}
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', 'ssssssssshhhhhhhhhhhh')
 
-# Celery
-BROKER_URL = os.environ['REDIS_URL']
-CELERY_RESULT_BACKEND = BROKER_URL
+if 'REDIS_URL' in os.environ:
+    # Celery
+    BROKER_URL = os.environ['REDIS_URL']
+    CELERY_RESULT_BACKEND = BROKER_URL
 
-if os.environ.get('MEMCACHIER_SERVERS'):
-    os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS')
-    os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME')
-    os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD')
+if 'MEMCACHE_URL' in os.environ:
+    memcache = SERVICES['memcached'][0]['credentials']
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHE_URL']
+    #os.environ['MEMCACHE_USERNAME'] = memcache['user']
+    #os.environ['MEMCACHE_PASSWORD'] = memcache['password']
